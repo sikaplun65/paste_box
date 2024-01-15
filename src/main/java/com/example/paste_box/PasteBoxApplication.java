10499bbb96e0ca06d00ade543267782fbe9108ca
sergiikaplun <sikaplun@gmail.com>
2024-01-15 11:33:19 +0200

    Initial commit

---
+++ src/main/java/com/example/paste_box/PasteBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.paste_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paste_box/PasteBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paste_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
